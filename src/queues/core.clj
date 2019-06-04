d967b6b734368adb88ed5f9feae6a07fa6156e83
Marcos Simões <marcos.simoes@nubank.com.br>
2019-06-04 18:08:05 -0300

    dequeue first version

--- src/queues/core.clj
+++ src/queues/core.clj
@@ -1,10 +1,23 @@
 (ns queues.core
   (:gen-class))
 
+(defn added-event
+  "Receives a map of agents and jobs asigned and an event
+  processes the event and adds the result to agents and "
+  [agents-and-jobs-asigned event])
+
 (defn dequeue
   "Receives a pool map of new_agents, job_requests and new-jobs
   Returns a map containing the job assignments to different agents"
-  [pool])
+  ([events]
+   (let [agents-and-jobs {:agents {}
+                          :jobs-assigned {}
+                          :jobs-waiting {}}]
+     (dequeue events
+              agents-and-jobs)))
+  ([events agents-and-jobs]
+   (-> (reduce (partial added-event) agents-and-jobs events)
+       (:jobs-assigned))))
 
 (defn -main
   [& args]
--- test/queues/core_test.clj
+++ test/queues/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [queues.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def new-agent-1
+  {
+   "new_agent": {
+                 "id": "8ab86c18-3fae-4804-bfd9-c3d6e8f66260",
+                     "name": "BoJack Horseman",
+                 "primary_skillset": ["bills-questions"],
+                 "secondary_skillset": []
+                 }
+   })
