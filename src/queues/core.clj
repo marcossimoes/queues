fe892135a89cc0fbdf1edbbb3fc1f270a533368e
Marcos Simões <marcos.simoes@nubank.com.br>
2019-06-05 11:56:58 -0300

    add basic deque design and tests

--- src/queues/core.clj
+++ src/queues/core.clj
@@ -4,20 +4,22 @@
 (defn added-event
   "Receives a map of agents and jobs asigned and an event
   processes the event and adds the result to agents and "
-  [agents-and-jobs-asigned event])
+  [agents-and-jobs event]
+  agents-and-jobs)
 
 (defn dequeue
   "Receives a pool map of new_agents, job_requests and new-jobs
   Returns a map containing the job assignments to different agents"
   ([events]
-   (let [agents-and-jobs {:agents {}
-                          :jobs-assigned {}
-                          :jobs-waiting {}}]
+   (let [agents-and-jobs {:agents []
+                          :jobs-assigned []
+                          :jobs-waiting []}]
      (dequeue events
               agents-and-jobs)))
   ([events agents-and-jobs]
-   (-> (reduce (partial added-event) agents-and-jobs events)
-       (:jobs-assigned))))
+   (->> events
+        (reduce (partial added-event) agents-and-jobs)
+        (:jobs-assigned))))
 
 (defn -main
   [& args]
