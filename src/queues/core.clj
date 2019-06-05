cd75addf2b73dcde99281f0ebe6e97e656bb0f88
Marcos Simões <marcos.simoes@nubank.com.br>
2019-06-05 14:57:52 -0300

    fix keywords in queues.core to be namespaced

--- src/queues/core.clj
+++ src/queues/core.clj
@@ -1,5 +1,6 @@
 (ns queues.core
-  (:require [queues.models.events :as events])
+  (:require [queues.models.events :as events]
+            [queues.models.agents-and-jobs :as aajs])
   (:gen-class))
 
 (defn added-event
@@ -9,22 +10,22 @@
   (let [type ((comp first keys) event)
         content ((comp first vals) event)]
     (case type
-      ::events/new-gent (update agents-and-jobs :agents #(conj % content))
+      ::events/new-gent (update agents-and-jobs ::aajs/agents #(conj % content))
       agents-and-jobs)))
 
 (defn dequeue
   "Receives a pool map of new_agents, job_requests and new-jobs
   Returns a map containing the job assignments to different agents"
   ([events]
-   (let [agents-and-jobs {:agents []
-                          :jobs-assigned []
-                          :jobs-waiting []}]
+   (let [agents-and-jobs {::aajs/agents []
+                          ::aajs/jobs-assigned []
+                          ::aajs/jobs-waiting []}]
      (dequeue events
               agents-and-jobs)))
   ([events agents-and-jobs]
    (->> events
         (reduce (partial added-event) agents-and-jobs)
-        (:jobs-assigned))))
+        (::aajs/jobs-assigned))))
 
 (defn -main
   [& args]
