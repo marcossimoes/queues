8e1dd93c068acb2115123421676d497a3f911902
Marcos Simões <marcos.simoes@nubank.com.br>
2019-07-19 17:02:54 -0300

    refactor added-event from method to case

--- src/queues/models/job.clj
+++
@@ -1,8 +0,0 @@
-(ns queues.models.job
-  (:require [clojure.spec.alpha :as s]))
-
-(s/def ::id string?)
-(s/def ::type string?)
-(s/def ::urgent boolean?)
-
-(s/def ::job (s/keys :req [::id ::type ::urgent]))
--- src/queues/models/job_assigned.clj
+++
@@ -1,6 +0,0 @@
-(ns queues.models.job-assigned
-  (:require [clojure.spec.alpha :as s]
-            [queues.models.job :as job]
-            [queues.models.job-request :as jr]))
-
-(s/def ::job-assigned (s/keys :req [::job/id ::jr/agent-id]))
